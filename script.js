7e21ece994b5072b1ca33743d16b973de1e141ac
TavrsD <dtavares@id.uff.br>
2025-02-13 23:11:20 -0300

    feat: add feedback messages section, script to create it and necessary IDs at the form elements.

---
+++ script.js
@@ -0,0 +1,18 @@
+function enviarFeedback() {
+    let inputName = document.querySelector("#name");
+    let inputMessage = document.querySelector("#feecback-message");
+   
+    let secao = document.querySelector(".FeedbackMessages");
+    let escopo = document.createElement("div");
+    let nome = document.createElement("h3");
+    let message = document.createElement("p");
+
+    nome.innerText = inputName.value;
+    message.innerText = inputMessage.value;
+    escopo.append(nome);
+    escopo.append(message);
+    secao.append(escopo);
+}
+
+let feedbackButton = document.querySelector("#send-feedback");
+button.addEventListener("click", enviarFeedback);
